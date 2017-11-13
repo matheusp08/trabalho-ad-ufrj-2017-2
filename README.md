dbdc80cbe0869c062b3310e65df645cfab8cb529
Unknown <matheuspinheiro93@gmail.com>
2017-11-13 09:16:46 -0200

    links no readme.md e add parametro rho

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # trabalho-ad-ufrj-2017-2
 UFRJ - Trabalho de AD 2017/2
+
+Trabalho exemplo
+- https://github.com/peterplupo/ad-dcc-ufrj-201101
+
+Google Style Python
+- http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
--- simulador/simulador.py
+++ simulador/simulador.py
@@ -5,23 +5,26 @@ import fila as f
 class Simulador:
     """Classe do simulador
     Args:
-        taxa_entrada: taxa de entrada do sistema.
-        n_rodadas: numero de rodadas da simulacao.
+        taxa_entrada: taxa de entrada do sistema
+        n_rodadas: numero de rodadas da simulacao
+        rho: taxa de servico
     Returns:
         nada
     """
-    def executar(self, taxa_entrada, n_rodadas):
+    def executar(self, taxa_entrada, n_rodadas, rho):
         """Funcao de execucao do simulador
         Args:
-            taxa_entrada: taxa de entrada do sistema.
-            n_rodadas: numero de rodadas da simulacao.
+            taxa_entrada: taxa de entrada do sistema
+            n_rodadas: numero de rodadas da simulacao
+            rho: taxa de servico
         Returns:
             nada
         """
         print("Numero de rodadas: %d" % n_rodadas)
+        print("Taxa de servico rho: %f" % rho)
         f.Fila("FCFS", 1, taxa_entrada)
         f.Fila("FCFS", 2, taxa_entrada)
 
 
 # INICIO DO PROGRAMA
-Simulador().executar(1.2, 100)
+Simulador().executar(1.2, 100, 0.2)
