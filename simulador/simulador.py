522579e3cedda8e53780d007eb859e0a663ac508
Mario Bonicenha <mario@uplay.com.br>
2017-11-22 00:33:10 -0200

    Adaptando o codigo para processar a fila na ordem correta e exibir os logs. Ainda nao sei se as medias batem.

--- simulador/simulador.py
+++ simulador/simulador.py
@@ -50,12 +50,18 @@ class Simulador:
         rodada_atual = 1
         eventos = []
         fregues_executando = Fregues()
-        while rodada_atual <= n_rodadas:
-            tempo_ate_prox_chegada = Utils.gera_taxa_exp_seed(lambd)
+        nao_tem_mais_ngm_p_chegar = 0
+        
+        
+        while fregueses_servidos < n_fregueses:
+            tempo_ate_prox_chegada = lambd
             tempo += tempo_ate_prox_chegada
-
-            while tempo_ate_prox_chegada > 0 and fregues_executando.fregues_id != -1:
-                if fregues_executando.tempo_restante < tempo_ate_prox_chegada:
+            print("Tempo de execução: ", tempo)
+            
+            while (tempo_ate_prox_chegada > 0 and fregues_executando.fregues_id != -1) or nao_tem_mais_ngm_p_chegar:
+                # print("While 2. Tempo: ", tempo, ". Tempo restante: ", fregues_executando.tempo_restante, ". PRox chegada: ", tempo_ate_prox_chegada);
+                print("ID:", fregues_executando.fregues_id, "Tempo restante: ", fregues_executando.tempo_restante)
+                if (fregues_executando.tempo_restante < tempo_ate_prox_chegada) or nao_tem_mais_ngm_p_chegar:
                     tempo_ate_prox_chegada -= fregues_executando.tempo_restante
                     fregues_executando.tempo_restante = 0
                     tempo_atual = tempo - tempo_ate_prox_chegada
@@ -64,8 +70,10 @@ class Simulador:
                             fregues_executando.fregues_id,
                             TipoEvento.FIM_SERVICO,
                             fregues_executando.prioridade))
+                    print("(Acabou seu servico.)\n")
                     
                     if fregues_executando.prioridade == 1:
+                        print("Botando fregues", fregues_executando.fregues_id, "na fila 2")
                         w1 = tempo_atual - fregues_executando.tempo_chegada1 - fregues_executando.tempo_servico1
                         fila1.atualiza_tempo_w(w1)
                         fila1.remove()
@@ -73,10 +81,13 @@ class Simulador:
                         fila2.adiciona(fregues_executando)
                         eventos.append(Evento(tempo_atual, fregues_executando.fregues_id, TipoEvento.CHEGADA, 2))
                     else:
+                        print("Fregues", fregues_executando.fregues_id , "indo embora..")
                         w2 = tempo_atual - fregues_executando.tempo_chegada2 - fregues_executando.tempo_servico2
                         fila2.atualiza_tempo_w(w2)
                         fila2.remove()
                         fregueses_servidos += 1
+                        if fregueses_servidos == n_fregueses:
+                            print("Acabaram os fregueses!!!")
                         if fregueses_servidos % n_fregueses == 0:
                             rodada_atual += 1
                     
@@ -87,19 +98,27 @@ class Simulador:
                             fregues_executando = fila2.proximo_fregues()
                         else:
                             fregues_executando = Fregues()
+                            nao_tem_mais_ngm_p_chegar = 0
+
                 else:
                     fregues_executando.tempo_restante -= tempo_ate_prox_chegada
                     tempo_ate_prox_chegada = 0
+                    if id_proximo_fregues == n_fregueses:
+                        #TENHO QUE ATUALIZAR O SISTEMA DIZENDO Q N TEM MAIS NGM PARA CHEGAR, QUE ESSE CARA QUE TEM Q RODAR.
+                        nao_tem_mais_ngm_p_chegar = 1
 
-            fregues = Fregues(id_proximo_fregues, tempo, taxa_servico, rodada_atual)
-            fila1.soma_servico_x(fregues.tempo_servico1)
-            fila2.soma_servico_x(fregues.tempo_servico2)
+            if id_proximo_fregues < n_fregueses:
+                # Pq diabos esse comeco estava fora desse if? Acaba nao tratando o caso em que tempo_restante = tempo_ate_prox_chegada !!
+                # print("Colocando novo fregues na fila 1. ID:", id_proximo_fregues)
+                fregues = Fregues(id_proximo_fregues, tempo, taxa_servico, rodada_atual)
+                fila1.soma_servico_x(fregues.tempo_servico1)
+                fila2.soma_servico_x(fregues.tempo_servico2)
 
-            fila1.atualiza_nq(fila1.tamanho())
-            fila2.atualiza_nq(fila2.tamanho())
+                fila1.atualiza_nq(fila1.tamanho())
+                fila2.atualiza_nq(fila2.tamanho())
+                fila1.adiciona(fregues)
+                eventos.append(Evento(tempo, id_proximo_fregues, TipoEvento.CHEGADA, 1))
 
-            fila1.adiciona(fregues)
-            eventos.append(Evento(tempo, id_proximo_fregues, TipoEvento.CHEGADA, 1))
             if fregues_executando.fregues_id == -1:
                 fregues_executando = fregues
             else:
@@ -108,14 +127,10 @@ class Simulador:
                     fila2.atualiza_nq(-1)
                     fila2.atualiza_ns(1)
                 else:
-                    if fregues_executando.prioridade == 2:
-                        fregues_executando = fregues
-                        fila2.atualiza_nq(-1)
-                        fila2.atualiza_ns(1)
-                    else:
-                        fila1.atualiza_nq(-1)
-                        fila1.atualiza_ns(1)
-            id_proximo_fregues += 1
+                    fila1.atualiza_nq(-1)
+                    fila1.atualiza_ns(1)
+            if id_proximo_fregues < n_fregueses:        
+                id_proximo_fregues += 1
                 
             if id_proximo_fregues % 10 == 0:
                 variancia_ns.append(fila1.calcula_variancia_ns(1, id_proximo_fregues))
@@ -126,6 +141,6 @@ class Simulador:
         fila1.imprime_esperancas()
         fila2.imprime_esperancas()
 
-Simulador().executar(10000, 1, 0.4)
-Plot().desenha_grafico(utilizacao, 'Numero de Fregueses', 'Utilizacao do Servidor')
-Plot().desenha_grafico(variancia_ns, 'Numero de Fregueses', 'Variancia de Ns')
+Simulador().executar(2, 1, 2)
+# Plot().desenha_grafico(utilizacao, 'Numero de Fregueses', 'Utilizacao do Servidor')
+# Plot().desenha_grafico(variancia_ns, 'Numero de Fregueses', 'Variancia de Ns')
